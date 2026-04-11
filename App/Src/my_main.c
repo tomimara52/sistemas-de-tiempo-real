9de21fb7e4fe74e64e8ae25b98f3e32716172b8a
Tomás Maraschio <tomas.maraschio@gmail.com>
2026-04-11 10:49:37 -0300

    blinky seteable pa

--- App/Src/my_main.c
+++ App/Src/my_main.c
@@ -1,23 +1,83 @@
 #include "FreeRTOS.h"
 #include "button.h"
+#include "semphr.h"
 #include "stm32h5xx_nucleo.h"
 #include "task.h"
+#include <stdint.h>
 
-void blinky(void *args) {
+typedef struct _blinky_admin_args {
+    SemaphoreHandle_t *sem;
+    TickType_t *tick_delay;
+    TaskHandle_t *blinky_handle;
+} BlinkyAdminArgs;
+
+SemaphoreHandle_t blinky_admin_sem;
+
+void blinky(void *ticks_delay_void) {
+
+    vTaskSuspend(NULL);
+
     for (;;) {
         BSP_LED_Toggle(LED_GREEN);
 
-        vTaskDelay(pdMS_TO_TICKS(250));
+        vTaskDelay(*((TickType_t *)ticks_delay_void));
     }
 }
 
-void USER_BUTTON_Callback() {
-    printf("hola\n");
+void blinky_admin(void *args_void) {
+    BlinkyAdminArgs args = *((BlinkyAdminArgs *)args_void);
+    uint8_t blinky_is_running = 1;
+    TickType_t button_press_tick = 0;
+
+    for (;;) {
+        xSemaphoreTake(*args.sem, portMAX_DELAY);
+
+        if (blinky_is_running || button_press_tick == 0) {
+            vTaskSuspend(*args.blinky_handle);
+            BSP_LED_On(LED_GREEN);
+
+            button_press_tick = xTaskGetTickCount();
+            printf("Setting blinky period (t=%lu)\n", button_press_tick);
+        } else {
+
+            TickType_t current_tick = xTaskGetTickCount();
+            *args.tick_delay = current_tick - button_press_tick;
+
+            vTaskResume(*args.blinky_handle);
+
+            printf("Blinky period set at %lu (t=%lu)\n", *args.tick_delay,
+                   current_tick);
+        }
+
+        blinky_is_running = !blinky_is_running;
+    }
+}
+
+void USER_BUTTON_Callback() { 
+    BaseType_t higher_prio_task_awoken = pdFALSE;
+
+    xSemaphoreGiveFromISR(blinky_admin_sem, &higher_prio_task_awoken);
+
+    portYIELD_FROM_ISR(higher_prio_task_awoken);
 }
 
 int my_main() {
-    UserButton_Init(GPIO_MODE_IT_RISING);
-    xTaskCreate(blinky, "blinky", 100, NULL, 0, NULL);
+    UserButton_Init(GPIO_MODE_IT_RISING_FALLING);
+
+    blinky_admin_sem = xSemaphoreCreateBinary();
+
+    static TickType_t blinky_ticks_delay = 0;
+
+    static TaskHandle_t blinky_handle;
+
+    static BlinkyAdminArgs blinky_admin_args;
+    blinky_admin_args.tick_delay = &blinky_ticks_delay;
+    blinky_admin_args.sem = &blinky_admin_sem;
+    blinky_admin_args.blinky_handle = &blinky_handle;
+
+    xTaskCreate(blinky, "blinky", 100, &blinky_ticks_delay, 0, &blinky_handle);
+    xTaskCreate(blinky_admin, "blinky admin", 1000, &blinky_admin_args, 1, NULL);
+
     vTaskStartScheduler();
 
     return 0;
