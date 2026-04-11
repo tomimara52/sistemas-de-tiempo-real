21613a1418d958d98f15b7d1876f42a7da294cf0
Tomás Maraschio <tomas.maraschio@gmail.com>
2026-04-11 14:57:04 -0300

    no escribir todo el path

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/cuda/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/usr/lib/rustup/bin:/home/tomi/.local/bin:/home/tomi/.ghcup/bin:/home/tomi/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.linux64_1.0.100.202602081740/tools/bin/ cmake --preset Debug
+PATH=$PATH:/home/tomi/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.linux64_1.0.100.202602081740/tools/bin/ cmake --preset Debug
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/opt/cuda/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/usr/lib/rustup/bin:/home/tomi/.local/bin:/home/tomi/.ghcup/bin:/home/tomi/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.linux64_1.0.100.202602081740/tools/bin/ cmake --build --preset Debug
+PATH=$PATH:/home/tomi/st/stm32cubeide_2.1.1/plugins/com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.14.3.rel1.linux64_1.0.100.202602081740/tools/bin/ cmake --build --preset Debug
