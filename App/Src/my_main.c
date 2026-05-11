0bdc0f1efae889f094e40e183b2955a370fb41e3
Tomás Maraschio <tomas.maraschio@gmail.com>
2026-05-11 20:33:21 -0300

    usar periodo internamente
    el rpm solo se calcula a la hora de imprimir, y eventualmente para
    mandar a la cola. pero toda la lógica del target y el buzzer se hace
    usando el período

--- App/Src/my_main.c
+++ App/Src/my_main.c
@@ -10,9 +10,15 @@
 // => frequency: 16Mhz / 16 = 1Mhz
 const uint32_t TIMER_FREQ = 1000000;
 const float TIMER_FREQ_FLOAT = (float)TIMER_FREQ;
+
 const uint32_t MAX_RPM = 6000; // 100 RPS
 const uint32_t MIN_PERIOD = (TIMER_FREQ * 60) / MAX_RPM;
-const float TARGET_THRESH = 0.1f;
+
+// have to be more than THRESH_PERCENT% away from target_rpm for buzzer to sound
+const uint32_t THRESH_PERCENT = 10;
+// period is inverse of rpm so limits are upside down
+const float PERIOD_THRESH_UPPER = 100.0f / (100.0f - (float)THRESH_PERCENT);
+const float PERIOD_THRESH_LOWER = 100.0f / (100.0f + (float)THRESH_PERCENT);
 
 QueueHandle_t rpm_queue = NULL;
 
@@ -28,41 +34,42 @@ typedef struct {
 
 void rpm_calc(void *args) {
     uint32_t prev_capture = 0;
-    float rpm = 0.0f;
-    float target_rpm = 0.0f;
+    uint32_t period = 0;
+    uint32_t target_period = 0;
     Event event;
 
     for (;;) {
-        TickType_t us_to_wait = target_rpm == 0.0f 
+        TickType_t us_to_wait = target_period == 0
                                     ? portMAX_DELAY
-                                    : (TIMER_FREQ_FLOAT / (target_rpm * (1.0 - TARGET_THRESH))) * 60;
-        if (xQueueReceive(rpm_queue, &event, pdMS_TO_TICKS(us_to_wait/1000)) == pdPASS) {
+                                    : ((float)target_period) * PERIOD_THRESH_UPPER;
+        if (xQueueReceive(rpm_queue, &event,
+                          pdMS_TO_TICKS(us_to_wait / 1000)) == pdPASS) {
 
             if (event.command == NEW_CAPTURE) {
                 uint32_t curr_capture = event.payload;
-                uint32_t period = curr_capture - prev_capture;
+                period = curr_capture - prev_capture;
 
                 if (period < MIN_PERIOD)
                     continue;
 
-                rpm = (TIMER_FREQ_FLOAT / period) * 60.0f;
+                float rpm = (TIMER_FREQ_FLOAT / period) * 60.0f;
                 prev_capture = curr_capture;
 
                 printf("rpm: %d.%02d\t", (int)rpm, ((int)(rpm * 100)) % 100);
                 printf("period: %d\n", period);
 
-                if (target_rpm != 0 &&
-                    (target_rpm - rpm) * (target_rpm - rpm) >
-                        rpm * rpm * TARGET_THRESH * TARGET_THRESH) {
+                if (target_period != 0 &&
+                    ((float)period > (float)target_period * PERIOD_THRESH_UPPER ||
+                     (float)period < (float)target_period * PERIOD_THRESH_LOWER)) {
                     HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
                 } else {
                     HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
                 }
             } else if (event.command == SET_TARGET) {
-                target_rpm = rpm;
+                target_period = period;
             }
         } else {
-            printf("target rpm: %d.%02d\t", (int)target_rpm, ((int)(target_rpm * 100)) % 100);
+            printf("target period: %d\t", target_period);
             printf("us_to_wait: %d\n", us_to_wait);
             HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
         }
