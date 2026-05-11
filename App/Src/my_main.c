3a72c0a5a80f98d47b995c91a177f3891157314a
Tomás Maraschio <tomas.maraschio@gmail.com>
2026-05-11 18:18:29 -0300

    comando SET_TARGET

--- App/Src/my_main.c
+++ App/Src/my_main.c
@@ -1,4 +1,5 @@
 #include "FreeRTOS.h"
+#include "button.h"
 #include "queue.h"
 #include "stm32h5xx_nucleo.h"
 #include "task.h"
@@ -11,6 +12,7 @@ const uint32_t TIMER_FREQ = 1000000;
 const float TIMER_FREQ_FLOAT = (float)TIMER_FREQ;
 const uint32_t MAX_RPM = 6000; // 100 RPS
 const uint32_t MIN_PERIOD = (TIMER_FREQ * 60) / MAX_RPM;
+const float TARGET_THRESH = 0.1f;
 
 QueueHandle_t rpm_queue = NULL;
 
@@ -26,6 +28,8 @@ typedef struct {
 
 void rpm_calc(void *args) {
     uint32_t prev_capture = 0;
+    float rpm = 0.0f;
+    float target_rpm = 0.0f;
     Event event;
 
     for (;;) {
@@ -38,18 +42,28 @@ void rpm_calc(void *args) {
             if (period < MIN_PERIOD)
                 continue;
 
-            float rpm = (TIMER_FREQ_FLOAT / period) * 60.0f;
+            rpm = (TIMER_FREQ_FLOAT / period) * 60.0f;
             prev_capture = curr_capture;
 
             printf("rpm: %d.%02d\t", (int)rpm, ((int)(rpm * 100)) % 100);
             printf("period: %d\n", period);
+
+            if (target_rpm != 0 &&
+                (target_rpm - rpm) * (target_rpm - rpm) >
+                    rpm * rpm * TARGET_THRESH * TARGET_THRESH) {
+                HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+            } else {
+                HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
+            }
+        } else if (event.command == SET_TARGET) {
+            target_rpm = rpm;
         }
     }
 }
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     if (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) {
-        Event capture_event = { .command = NEW_CAPTURE };
+        Event capture_event = {.command = NEW_CAPTURE};
         capture_event.payload = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
 
         BaseType_t xHigherPriorityTaskWoken;
@@ -60,8 +74,20 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
     }
 }
 
+void USER_BUTTON_Callback() {
+    Event set_target_event = {.command = SET_TARGET};
+
+    BaseType_t xHigherPriorityTaskWoken;
+
+    xQueueSendFromISR(rpm_queue, &set_target_event, &xHigherPriorityTaskWoken);
+
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
 int my_main() {
     rpm_queue = xQueueCreate(10, sizeof(Event));
+
+    UserButton_Init(GPIO_MODE_IT_RISING);
 
     xTaskCreate(rpm_calc, "rpm", 300, NULL, 0, NULL);
 
